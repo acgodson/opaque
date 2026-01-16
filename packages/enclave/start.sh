4c8612bd9c639fef837cc1eebb03551292f70d77
acgodson <anigodson20@gmail.com>
2026-01-16 13:12:29 +0100

    Fix paths in start.sh

--- packages/enclave/start.sh
+++ packages/enclave/start.sh
@@ -3,10 +3,10 @@ echo "opaque Enclave Starting..."
 
 ip link set dev lo up
 
-node --max-old-space-size=1536 --stack-size=2048 --trace-warnings /app/packages/enclave/dist/index.js &
+node --max-old-space-size=1536 --stack-size=2048 --trace-warnings /app/dist/index.js &
 NODE_PID=$!
 
-python3 /app/packages/enclave/vsock-bridge.py &
+python3 /app/vsock-bridge.py &
 PYTHON_PID=$!
 
 wait $NODE_PID $PYTHON_PID
